07b46c27b6fa034a81b69404a7ef2752994e2356
Dan Persa <dan.persa@gmail.com>
2015-10-14 12:08:37 +0200

    Added list of scopes in config

--- src/main/scala/org/zalando/spearheads/innkeeper/oauth/OAuthDirectives.scala
+++ src/main/scala/org/zalando/spearheads/innkeeper/oauth/OAuthDirectives.scala
@@ -24,7 +24,9 @@ trait OAuthDirectives {
     }
 
   def hasOneOfTheScopes(authorizedUser: AuthorizedUser)(scope: Scope*): Directive0 = {
-    authorizedUser.scope.toSet.intersect(scope.toSet).isEmpty match {
+    println(s"scope: ${authorizedUser.scope}")
+    val configuredScopeNames: Set[String] = scope.flatMap(_.scopeNames).toSet
+    authorizedUser.scope.scopeNames.toSet.intersect(configuredScopeNames).isEmpty match {
       case false => pass
       case _     => reject(AuthorizationFailedRejection)
     }
