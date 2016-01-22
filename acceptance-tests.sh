bc60c5685ef16b69a305898f20db0aa1d0830f39
Dan Persa <dan.persa@gmail.com>
2016-01-22 15:33:37 +0100

    Create new profile for acceptance tests to prevent name clash with integration tests

--- acceptance-tests.sh
+++ acceptance-tests.sh
@@ -5,6 +5,6 @@ docker run -d --name db -p 5433:5432 -e POSTGRES_PASSWORD= -e POSTGRES_USER=innk
 sbt assembly
 docker build -t pierone.stups.zalan.do/pathfinder/innkeeper:latest-SNAPSHOT .
 docker run -d --link oauth:oauth --link db:db -p 8080:8080 \
-    -e INNKEEPER_ENV=test -e SCHEMA_RECREATE=true \
+    -e INNKEEPER_ENV=acceptance-test -e SCHEMA_RECREATE=true \
     -t pierone.stups.zalan.do/pathfinder/innkeeper:latest-SNAPSHOT
 sbt it:test
